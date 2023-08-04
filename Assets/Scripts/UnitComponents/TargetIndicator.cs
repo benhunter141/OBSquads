845eea4810c0b12e5ab78250f02455529df61afc
Ben Hunter <benhunter141@gmail.com>
2023-08-04 12:10:42 -0700

    Target indicator rotation error fixed
    Health/Damage/Attacks next

--- Assets/Scripts/UnitComponents/TargetIndicator.cs
+++ Assets/Scripts/UnitComponents/TargetIndicator.cs
@@ -13,6 +13,7 @@ public class TargetIndicator : MonoBehaviour
 
     private void Update()
     {
+        //fix in place below ball:
         transform.position = transform.parent.position - new Vector3(0, 0.5f, 0);
         transform.rotation = Quaternion.identity;
 
@@ -25,10 +26,16 @@ public class TargetIndicator : MonoBehaviour
 
     void PointAtTarget()
     {
+        //should be flat-forward towards target and
+        //should be at same pos.y as parent
         if (!pointerAnchor.activeSelf) pointerAnchor.SetActive(true);
+
         Vector3 centre = transform.position;
         Vector3 displacement = target.transform.position - centre;
-        pointerAnchor.transform.rotation = Quaternion.LookRotation(displacement);
+        displacement.y = 0;
+        //make disaplcement flat
+
+        pointerAnchor.transform.rotation = Quaternion.LookRotation(displacement, Vector3.up);
     }
     public void SetTarget(BaseUnit _target)
     {
